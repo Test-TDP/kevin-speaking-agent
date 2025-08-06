61bc5142d0850ee05d2633bc7fcd82584edcf897
Test-TDP <rruk24@gmail.com>
2025-08-06 15:16:32 -0500

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,9 @@
+// pages/index.tsx
+export default function Home() {
+  return (
+    <div>
+      <h1>Kevin Speaking Agent</h1>
+      <p>This is the homepage. The API is working!</p>
+    </div>
+  );
+}
