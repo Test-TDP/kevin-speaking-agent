512a340e7ab21d5c89e499b5b3017fae53426a91
Test-TDP <rruk24@gmail.com>
2025-08-06 15:25:13 -0500

    Update and rename index.tsx to in.tsx

--- index.tsx
+++
@@ -1,9 +0,0 @@
-// pages/index.tsx
-export default function Home() {
-  return (
-    <div>
-      <h1>Kevin Speaking Agent</h1>
-      <p>This is the homepage. The API is working!</p>
-    </div>
-  );
-}
